9c0ff606f94830db91166e128f23e0f797a75d1c
Vitalii-Nekhrest <79789216+Vitalii-Nekhrest@users.noreply.github.com>
2021-08-16 20:22:00 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,29 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as ec
+
+driver = webdriver.Chrome()
+driver.get('http://petfriends1.herokuapp.com/login')
+
+@pytest.fixture(autouse=True)
+def testing():
+   pytest.driver = webdriver.Chrome('C:/Program Files/Dr/chromedriver')
+   # Переходим на страницу авторизации
+   pytest.driver.get('http://petfriends1.herokuapp.com/login')
+
+   yield
+
+   pytest.driver.quit()
+
+
+def test_show_my_pets():
+   # Вводим email
+   pytest.driver.find_element_by_id('email').send_keys('vasya@mail.com')
+   # Вводим пароль
+   pytest.driver.find_element_by_id('pass').send_keys('12345')
+   # Нажимаем на кнопку входа в аккаунт
+   pytest.driver.find_element_by_css_selector('button[type="submit"]').click()
+   # Проверяем, что мы оказались на главной странице пользователя
+   assert pytest.driver.find_element_by_tag_name('h1').text == "PetFriends"
